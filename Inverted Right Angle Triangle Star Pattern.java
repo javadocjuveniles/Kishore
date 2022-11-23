7fff537165caf05b9c9be02f4d36017ac0f75ce0
javadocjuveniles <118755627+javadocjuveniles@users.noreply.github.com>
2022-11-23 19:04:35 +0530

    Create Inverted Right Angle Triangle Star Pattern.java

---
+++ Inverted Right Angle Triangle Star Pattern.java
@@ -0,0 +1,27 @@
+package com.kishore;
+
+import java.util.Scanner;
+
+public class pac {
+
+    public static void main(String[] args) {
+        Scanner get = new Scanner(System.in);
+        int a = get.nextInt();
+        for (int i = 1; i <= a; ++i) {
+            for (int j = a; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+output
+
+5
+
+*****
+****
+***
+**
+*
